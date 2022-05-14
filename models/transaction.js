dcdfdf8cceb8ebaccd7a9b778b1874937416f3d6
khagerman <71734063+khagerman@users.noreply.github.com>
2022-05-13 20:01:36 -0500

    added spend static method to transaction model

--- models/transaction.js
+++ models/transaction.js
@@ -1,8 +1,12 @@
 const transactions = require("../fakeDB");
 
+const { usePoints, totalPerCompany } = require("../helpers/helperFunctions");
 class Transaction {
   static async getAll() {
     return transactions;
+  }
+  static async getTotal() {
+    return totalPerCompany(transactions);
   }
   static async create(payer, points) {
     let newTransaction = {
@@ -14,6 +18,13 @@ class Transaction {
     return newTransaction;
   }
 
-  static async spend(points) {}
+  static async spend(points) {
+    let pointsUsed = usePoints(points);
+    //loop through subtracted amounts, add new  transaction to list of transaction
+    for (let transaction of points) {
+      await Transaction.create(transaction.payer, transaction.points);
+    }
+    return pointsUsed;
+  }
 }
 module.exports = Transaction;
